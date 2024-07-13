181449432a20c77a8fdbaa5ef8dbc54205beff40
Ahmed Fathy <ahmedfathi20044002@gmail.com>
2024-07-13 14:18:18 +0300

    wonderful begining on Main form :zap:

---
+++ Al-Nada Manager App/AF.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Al_Nada_Manager_App
+{
+    public partial class AF : Form
+    {
+        public AF()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (MainAF.Instance != null)
+            {
+                guna2MessageDialog1.Parent = MainAF.Instance;
+            }
+        }
+
+        private void AF_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Al-Nada Manager App/MainAF.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Al_Nada_Manager_App
+{
+    public partial class MainAF : AF
+    {
+        private static MainAF _obj;
+        public MainAF()
+        {
+            InitializeComponent();
+            _obj = this;
+        }
+
+        public static MainAF Instance
+        {
+            get
+            {
+                if (_obj == null)
+                {
+                    _obj = new MainAF();
+                }
+                return _obj;
+            }
+        }
+
+        private void MainAF_Load(object sender, EventArgs e)
+        {
+            _obj = this;
+            Maxbtn.PerformClick();
+        }
+
+        private void Exitbtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void guna2Panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
